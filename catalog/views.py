fd8713dca36461b65957c509ce0b43a9543f78eb
dedvnutree <tkachuk-p@mail.ru>
2021-05-12 20:08:45 +0500

    session test num_visits

--- catalog/views.py
+++ catalog/views.py
@@ -10,13 +10,18 @@ def index(request):
     num_brands = Brand.objects.count()
     num_brands_from_Russia = Brand.objects.filter(country__startswith='Russia').count()
 
+    num_visits = request.session.get('num_visits', 1)
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'num_furniture': num_furniture,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_brands': num_brands,
         'num_brands_from_Russia': num_brands_from_Russia,
+        'num_visits': num_visits,
     }
+
 
     return render(request, 'index.html', context=context)
 
