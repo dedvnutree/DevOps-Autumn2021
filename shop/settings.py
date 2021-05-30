debcbb34265d885d43752f8b8d4b8f821a4fcd64
dedvnutree <tkachuk-p@mail.ru>
2021-05-31 01:27:31 +0500

    fix siteid

--- shop/settings.py
+++ shop/settings.py
@@ -144,7 +144,7 @@ AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 
-SITE_ID = 1
+SITE_ID = 2
 
 servers = os.environ['MEMCACHIER_SERVERS']
 username = os.environ['MEMCACHIER_USERNAME']
